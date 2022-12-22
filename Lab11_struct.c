fa75b594a1609f447c006bba6082c22e243c7f1e
Andromedde <88080821+Andromedde@users.noreply.github.com>
2022-12-22 15:19:56 +0300

    Create Lab11_struct.c

---
+++ Lab11_struct.c
@@ -0,0 +1,16 @@
+//
+// Created by FerHard_GW on 19.12.2022.
+//
+
+#ifndef UNTITLED2_LAB11_H
+#define UNTITLED2_LAB11_H
+struct Cord{
+    int x1, y1;
+    int x2, y2;
+    int x3, y3;
+    int x4, y4;
+};
+
+int Area(struct Cord arg);
+float Perimeter(struct Cord arg);
+#endif //UNTITLED2_LAB11_H
